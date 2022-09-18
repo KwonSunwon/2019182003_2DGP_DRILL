9da310492af1d76dda08e707ab7de0b6a1e819b8
KwonSunwon <kso00011147@gmail.com>
2022-09-18 21:25:00 +0900

    drill 05 완성
    2019182003 권순원

---
+++ 05/tutleMoveGame.py
@@ -0,0 +1,34 @@
+import turtle
+
+def moveLeft():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def moveRight():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def moveUp():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def moveDown():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+turtle.onkey(moveLeft, 'a')
+turtle.onkey(moveRight, 'd')
+turtle.onkey(moveUp, 'w')
+turtle.onkey(moveDown, 's')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
+
+turtle.exitonclick()
